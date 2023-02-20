18c602593aa49d5236c3768b532c69cd54fbe008
TOsCodes <temitopeosieke@gmail.com>
2023-02-20 00:43:44 -0800

    Task variable C-programming

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,28 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 /**
 * main - Entry point
 *
 * Return: Always 0 (Success)
 *
 */
-
 int main(void)
 {
-	int n;
+int n;
 
-	if (n > 0)
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n > 0)
 {
-	printf("%d is positive\n", n);
+printf("%d is positive\n", n);
 }
-
-	else if (n == 0)
+else if (n == 0)
 {
-	printf("%d is zero\n", n);
+printf("%d is zero\n", n);
 }
-	else
+else
 {
-	printf("%d is negative\n", n);
+printf("%d is negative\n", n);
 }
-
 return (0);
 }
