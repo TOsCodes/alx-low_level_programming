f8201f2d92632d5ea7d73f4a582d7c851a5a773f
TOsCodes <temitopeosieke@gmail.com>
2023-02-19 17:43:30 -0800

    variables task 1 done

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,26 @@
+#include <studio.h>
+/**
+  * main - Entry point
+  *
+  * Return: Always 0 (Success)
+  */
+
+int main(void)
+{
+
+	if (n > 0)
+{
+	printf("n is positive%d\n", n);
+}
+
+	else if (n < 0)
+{
+	printf("n is negative%d\n", n);
+}
+	else
+{
+	printf("n is zero%d\n", n);
+}
+
+return (0)
+}
