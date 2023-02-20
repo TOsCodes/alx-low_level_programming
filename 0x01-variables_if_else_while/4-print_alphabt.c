205a3f52ad933f0587ec220e6b9828c573fcc59c
TOsCodes <temitopeosieke@gmail.com>
2023-02-20 04:14:48 -0800

    Task variable C-programming

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+ #include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char alpha[] = "abcdfghijklmnoprstuvwxyz";
+	int dafe;
+
+	for (dafe = 0; dafe < 24; dafe++)
+	putchar(alpha[dafe]);
+	putchar('\n');
+
+return (0);
+}
