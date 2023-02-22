7bb4da6c3ef2219de8ca2983e95b671778832577
TOsCodes <temitopeosieke@gmail.com>
2023-02-21 19:20:07 -0800

    Task variable C-programming

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* main - Entry point
+* print alphabet in lower case and a new line
+* Return: Always 0 (Success)
+*
+*/
+
+void print_alphabet(void)
+{
+	char(ch);
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+	_putchar(ch);
+}
+	_putchar('\n');
+
+}
