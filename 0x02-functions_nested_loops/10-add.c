3d964c3d397b7ab60de03a5ae5db8d46c5fdbb8a
TOsCodes <temitopeosieke@gmail.com>
2023-02-23 13:47:34 -0800

    Task functions, nested loops C-programming

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* add - adds two integers (int, int)
+* @i: first parameter
+* @j: second parameter
+* Return: k
+*/
+int add(int i , int j)
+{
+	int k;
+
+	(k = i + j);
+	return (k);
+}
+
