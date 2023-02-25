5b86013ce7d0b15f2db30f73262d4deaab044ebf
TOsCodes <temitopeosieke@gmail.com>
2023-02-24 21:01:27 -0800

    more functions and nested loops C-programming

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,22 +1,22 @@
 #include "main.h"
 
 /**
-* more_numbers - prints numbers between 0 to 14
-* 10 times.
-* Return: no return.
-*/
+ * more_numbers - print more numbers
+ */
+
 void more_numbers(void)
 {
 	int i, j;
 
-		for (i = 0; i < 10; i++)
+	for (i = 1; i <= 10; i++)
 	{
-			for (j = 0; j < 15; j++)
-			{
-				if (j >= 10)
-					_putchar('1');
-				_putchar((j % 10 + '0');
-			}
-			_putchar('\n');
+		for (j = 0; j <= 14; j++)
+		{
+			if (j >= 10)
+				_putchar('1');
+			_putchar (j % 10 + '0');
+		}
+		_putchar('\n');
 	}
 }
+
