e40033515bf2dc91171a70859b60757f342a8695
TOsCodes <temitopeosieke@gmail.com>
2023-02-24 20:04:33 -0800

    more functions and nested loops C-programming

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,17 +1,17 @@
 #include "main.h"
+
 /**
-* print_numbers- prints numbers 0 to 9 followed by a new line
-* Return: no return
+* print_numbers - prints numbers between 0 to 9.
+* Return: no return.
 */
 void print_numbers(void)
 {
 	int ch;
 
-	for (ch = 48 ; ch <= 57 ; i++)
-	{
+		for (ch = 48 ; ch < 58 ; ch++)
+{
 		_putchar(ch);
-	}
-
-	_putchar("\n");
+}
+		_putchar('\n');
 }
 
