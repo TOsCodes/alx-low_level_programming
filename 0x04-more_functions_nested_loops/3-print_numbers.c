f30c14127e14c7375581628248941fc8daa1a805
TOsCodes <temitopeosieke@gmail.com>
2023-02-24 16:13:41 -0800

    more functions and nested loops C-programming

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* print_numbers- prints numbers 0 to 9 followed by a new line
+* Return: 0 on success
+*/
+void print_numbers(void)
+{
+	int z;
+
+	for (z >= 48 ; z <= 57 ; i++)
+	{
+		_putchar(z);
+	}
+
+	{
+		_putchar("\n");
+	}
+
+return (0);
+}
+
